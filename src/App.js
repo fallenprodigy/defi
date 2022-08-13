5923b148cb260ae21b8e32f2a38a4b47d41f5bb9
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-08-13 18:50:06 +0400

    done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import About from "./components/about/About";
 import Developer from "./components/developer/Developer";
+import Footer from "./components/footer/Footer";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
 import Subscribe from "./components/subscribe/Subscribe";
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Developer />
       <Subscribe />
+      <Footer />
     </>
   );
 }
