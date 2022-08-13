a7f50b1b8bf395c0bd8c97f4da6b16eb0d96a3cf
GiorgiKalatozishvili <giorgi.kalatozishvili.1@btu.edu.ge>
2022-08-13 18:30:06 +0400

    subscribe section

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/about/About";
 import Developer from "./components/developer/Developer";
 import Hero from "./components/hero/Hero";
 import Navbar from "./components/navbar/Navbar";
+import Subscribe from "./components/subscribe/Subscribe";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Hero />
       <About />
       <Developer />
+      <Subscribe />
     </>
   );
 }
